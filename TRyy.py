63284cd87cc3a18fa4da034899cc145ab392738b
unalertu <240408016@st.biruni.edu.tr>
2025-02-13 19:47:38 +0300

    Update TRyy.py

--- TRyy.py
+++ TRyy.py
@@ -1,5 +1,13 @@
 import requests
 import json  # JSON formatında düzgün çıktı almak için
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
 
 url = "https://randomfox.ca/floof"
 response = requests.get(url)

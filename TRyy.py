af106b4084ac685ce3dbcf28c1138c19e3a2142c
unalertu <240408016@st.biruni.edu.tr>
2025-02-14 11:16:45 +0300

    Update TRyy.py

--- TRyy.py
+++ TRyy.py
@@ -6,7 +6,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
+    url = "https://randomfox.ca/floof"
+    response = requests.get(url)
+    print(response.status_code)
+    print(response.text)
+    fox = response.json()
+    print(fox["image"])
+    return fox
 
 
 url = "https://randomfox.ca/floof"
